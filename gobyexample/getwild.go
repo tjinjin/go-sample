31e51025a3c5fc347cd3a071d91222056398bade
tanaka masato <tanaka@feedforce.jp>
2016-12-21 09:13:36 +0900

    getwildAndTough!!!

--- gobyexample/getwild.go
+++ gobyexample/getwild.go
@@ -4,13 +4,32 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os/exec"
 	"sync"
 	"time"
 )
 
 var (
 	getWildWords = []string{"G", "e", "t", "W", "i", "l", "d"}
+	g            string
 )
+
+const (
+	textGetWild = `
+  ____      _    __        ___ _     _
+ / ___| ___| |_  \ \      / (_) | __| |
+| |  _ / _ \ __|  \ \ /\ / /| | |/ _  |
+| |_| |  __/ |_    \ V  V / | | | (_| |
+ \____|\___|\__|    \_/\_/  |_|_|\__,_|
+`
+)
+
+// 比較用に答えを作っておく
+func init() {
+	for _, v := range getWildWords {
+		g += v
+	}
+}
 
 func main() {
 	// goroutinの終了タイミングをあわせる
@@ -19,17 +38,21 @@ func main() {
 	c := make(chan string)
 
 	for {
-		r := ""
+		w := ""
 		for i := 0; i < 7; i++ {
 			// wait groupを追加
 			wg.Add(1)
 			go getwildAndTough(wg, c)
-			r += <-c
+			w += <-c
 		}
 		// WaitGroupの完了待ちここではforが7回回るので7つ待つ
 		wg.Wait()
-		fmt.Println(r)
-		r = ""
+		if isGetWild(w) {
+			fmt.Println(textGetWild)
+			exec.Command("say", "-v", "Ava", w).Run()
+			break
+		}
+		w = ""
 	}
 	close(c)
 }
@@ -41,4 +64,11 @@ func getwildAndTough(wg *sync.WaitGroup, c chan string) {
 	rand.Seed(time.Now().UnixNano())
 	// Intn(n) で0からnまでの数字からランダムで数字を選ぶ
 	c <- getWildWords[rand.Intn(len(getWildWords))]
+}
+
+func isGetWild(w string) bool {
+	if g == w {
+		return true
+	}
+	return false
 }
