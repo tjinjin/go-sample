cc387f7c829ec9b983af2680630924f1e1b87dc6
tanaka masato <tanaka@feedforce.jp>
2016-12-21 09:07:06 +0900

    Improve goroutines

--- gobyexample/getwild.go
+++ gobyexample/getwild.go
@@ -4,6 +4,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -12,23 +13,31 @@ var (
 )
 
 func main() {
+	// goroutinの終了タイミングをあわせる
+	wg := &sync.WaitGroup{}
 	// string型のチャネル
 	c := make(chan string)
-	w := ""
 
 	for {
-		// gorutines
-		go getwildAndTough(c)
-		// チャネルに結果を送信
-		w += <-c
-		if len(w) == len(getWildWords) {
-			fmt.Println(w)
-			w = ""
+		r := ""
+		for i := 0; i < 7; i++ {
+			// wait groupを追加
+			wg.Add(1)
+			go getwildAndTough(wg, c)
+			r += <-c
 		}
+		// WaitGroupの完了待ちここではforが7回回るので7つ待つ
+		wg.Wait()
+		fmt.Println(r)
+		r = ""
 	}
+	close(c)
 }
 
-func getwildAndTough(c chan string) {
+func getwildAndTough(wg *sync.WaitGroup, c chan string) {
+	// WaitGroupが完了したことを通知
+	// deferは関数終了時に実行したい書くときに使う
+	defer wg.Done()
 	rand.Seed(time.Now().UnixNano())
 	// Intn(n) で0からnまでの数字からランダムで数字を選ぶ
 	c <- getWildWords[rand.Intn(len(getWildWords))]
