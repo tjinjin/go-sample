997571fe6504db7d706f97e0caf8717dbb0e6e83
tjinjin <tjinjinprogram@gmail.com>
2017-11-13 22:52:56 +0900

    20171113

---
+++ starting_golang/fmt/printf.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d %b %o %x\n", 17, 17, 17, 17)
+	fmt.Printf("%d %d %d \n", 17, 17)
+	fmt.Printf("%d %d %d \n", 17, 17, 17, 17)
+}
---
+++ starting_golang/var/var.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		n = 1
+		s = "string"
+		b = true
+	)
+
+	fmt.Println(n, s, b)
+}
